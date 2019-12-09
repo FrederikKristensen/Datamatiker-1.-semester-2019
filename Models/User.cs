facfaea62fb786d4610bcc286a52cd8b86eee119
Rasmus Rohde Bergstrøm <rasm915w@edu.easj.dk>
2019-12-09 12:36:18 +0100

    test

---
+++ Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lplplp.Models
+{
+    public class User
+    {
+        private int id;
+        private string userName;
+        private string passWord;
+
+        public int Id { get => id; set => id = value; }
+        public string UserName { get => userName; set => userName = value; }
+        public string PassWord { get => passWord; set => passWord = value; }
+
+        public User()
+        {
+        }
+        public User (string UserName, string PassWord)
+        {
+            this.UserName = UserName;
+            this.PassWord = PassWord;
+        }
+        public bool CheckInformation()
+        {
+            if (!this.UserName.Equals("") && !this.PassWord.Equals(""))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ View/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using lplplp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace lplplp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        //void SignInProcedure(object sender,  EventArgs e)
+        //{
+        //    User user = new User(Entry_Username.Text, Entry_Password.Password);
+        //    if (user.CheckInformation())
+        //    {
+        //        MessageDialog("Login", "Login Sucess", "Derp");
+        //    }
+        //    else
+        //    {
+        //        DisplayAlert("Login", "Login Not Correct, empty username or password.", "derp");
+        //    }
+        //}
+        private async void PopUpMethod()
+        {
+            ContentDialog dialogue = new ContentDialog()
+            {
+                Title = "Log in",
+                Content = "Signed in successfully",
+                CloseButtonText = "Ok",
+            };
+
+            await dialogue.ShowAsync();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PopUpMethod();
+        }
+    }
+}
